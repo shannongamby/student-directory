83253e74a7f9064e4ad637d491673b0d060c6773
Shannon Gamby <shannongamby@hotmail.com>
2018-09-28 01:17:02 +0100

    Refactoring the interactive menu

--- directory.rb
+++ directory.rb
@@ -1,23 +1,24 @@
+@students = []
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return"
-  students = []
-  name = gets.tr("\n", "")
+  name = gets.chomp
   while !name.empty? do
     puts "And the cohort?"
-    cohort = gets.tr("\n", "")
+    cohort = gets.chomp
     if cohort == ""
        cohort = "october"
     end
-    students << {name: name, cohort: cohort.to_sym, country: :UK}
-    if students.length == 1
-      puts "Now we have #{students.count} student. Enter another name."
+    @students << {name: name, cohort: cohort.to_sym, country: :UK}
+    if @students.length == 1
+      puts "Now we have #{@students.count} student. Enter another name."
     else
-      puts "Now we have #{students.count} students. Enter another name."
+      puts "Now we have #{@students.count} students. Enter another name."
     end
-    name = gets.tr("\n", "")
+    name = gets.chomp
   end
-  return students
+  return @students
 end
 
 def print_header
@@ -25,49 +26,59 @@ def print_header
   puts "-------------".center(50)
 end
 
-def print(students)
+def print_student_list
   grouped = {}
-  students.each { |h|
+  @students.each { |h|
   if grouped.keys.include? h[:cohort]
     grouped[h[:cohort]].push(h[:name])
   else
     grouped[h[:cohort]] = [h[:name]]
   end
   }
-
   grouped.each { |cohort, names|
     puts "#{cohort} cohort: ".center(50)
     names.each { |name| puts name.center(50) }
+    puts " "
   }
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(50)
 end
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
   end
 end
 
 interactive_menu
 print_header
-print(students)
-print_footer(students)
+print_student_list
+print_footer
