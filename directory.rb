258a448bfa3c70eacb45e1675d0c09718cfb6bf9
Shannon Gamby <shannongamby@hotmail.com>
2018-09-28 01:34:25 +0100

    Added ability to save student data

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ def input_students
     if cohort == ""
        cohort = "october"
     end
-    @students << {name: name, cohort: cohort.to_sym, country: :UK}
+    @students << {name: name, cohort: cohort.to_sym}
     if @students.length == 1
       puts "Now we have #{@students.count} student. Enter another name."
     else
@@ -56,6 +56,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -71,11 +72,23 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each { |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  }
+  file.close
 end
 
 interactive_menu
